6967da75a3bfea29a82ba1a9ca20f98e952962bb
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-09-29 21:14:32 -0400

    a third problem added

--- src/Arrays.java
+++ src/Arrays.java
@@ -14,7 +14,12 @@ public class Arrays {
 		//Q2
 		System.out.println(permutation1("Gab", "bag"));  //false
 		System.out.println(permutation1("gab", "bag"));  //true
-		System.out.println(permutation1("gab", "bag "));  //true
+		System.out.println(permutation1("gab", "bag "));  //false
+		
+		//Q3
+		//when you feel like it, write a way to call the method and get a result
+		
+		//Q4
 
 	}
 	
@@ -64,5 +69,68 @@ public class Arrays {
 		}
 		return sort1(s).equals(sort1(t));
 	}
+	
+	//solution 2:
+	//TO DO
+	
+	
+	//Q3: URLify: replace all spaces in a string with "%20"
+	
+	//tips: this is good to work from the end to the start
+	//treat the string as an array of chars
+	//go through array twice: once to count spaces (so you know total length of new string)
+	//next to replace characters
+		
+	//fyi "true length" of a string means ignoring EXTRA while space
+	//like "Mr John Smith     " is 13. pass that as trueLength
+	
+	public void replaceSpaces (char[] str, int trueLength) {
+		//int spaceCount = 0, index, i = 0;
+		int spaceCount = 0;
+		int index;
+		int i = 0;
+		
+		//count spaces:
+		for (i = 0; i < trueLength; i++) {
+			if (str[i] == ' ') {
+				spaceCount++;
+			}
+		}
+		
+		index = trueLength + spaceCount * 2; //adds true length plus extra room for new chars
+		
+		if (trueLength <str.length) {
+			str[trueLength] = '\0'; //that will end the array
+		}
+		
+		//this part will go backwards through the now-shortened string
+		//starts with last char and loops
+		//it decrements i while counting the original string
+		//and index when "creating" the new string with %20 inserted
+		//with each char, it replaces (or not) and converts from str[i] to str[index]
+		for (i = trueLength - 1; i>= 0; i--) {
+			if (str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+				index = index -3;
+			}
+			else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+	}
+	
+	
+	//Q4 check whether a string is a permutation of a palindrome
+	
+	//tips: understand both of those terms.
+	//palindrome: contains pairs of matching letters (with possibly ONE nonmatch)
+	//inefficient: creating all possible permutations and checking if palindromes. don't do it.
+	
+	//solution 1:
+	
+	
 	
 }
